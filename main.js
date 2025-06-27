6dbad0a112a62fdda226e841051842ae9a8d25c9
Baffour Brenya Agyenim Boateng <baffourbrenya@gmail.com>
2025-06-27 16:18:02 +0100

    Update main.js
    Added new faetures and removed unneccessary events.

--- main.js
+++ main.js
@@ -5,28 +5,33 @@ function showContent(group) {
     allGroups.forEach((content) => {
       content.classList.add('hidden');
     });
-    
+
     selectedGroup.classList.remove('hidden');
   } else {
-    
+
     selectedGroup.classList.add('hidden');
   }
 }
-  let currentSlide = 0;
+
+
+let currentSlide = 0;
 const slides = document.querySelectorAll('.image2');
 const slider = document.querySelector('.imager');
 function moveSlide(direction) {
+  const slideWidth = slides[0].offsetWidth;
   currentSlide = (currentSlide + direction + slides.length) % slides.length;
-  const offset = -currentSlide * 420; 
+  const offset = -currentSlide * slideWidth;
   slider.style.transform = `translateX(${offset}px)`;
 }
+
+
 function toggleTheme() {
   const body = document.body;
   const themeIcon = document.getElementById('toggle');
 
   body.classList.toggle('dark-mode');
   if (body.classList.contains('dark-mode')) {
-    themeIcon.textContent = '☼'; 
+    themeIcon.textContent = '☼';
   } else {
     themeIcon.textContent = '☽';
   }
@@ -38,50 +43,62 @@ document.addEventListener('DOMContentLoaded', () => {
   const themeIcon = document.getElementById('theme-icon');
   if (theme === 'dark') {
     document.body.classList.add('dark-mode');
-    themeIcon.textContent = '☼'; 
+    themeIcon.textContent = '☼';
   } else {
-    themeIcon.textContent = '☽'; 
+    themeIcon.textContent = '☽';
   }
 });
+
+
+const dropmenu = document.getElementById('drop');
+const fullScreenexit = document.querySelector('.fullscreen-exit');
+const menuIcon = document.querySelector('.bi-menu-button-wide')
+const closeBtn = document.querySelector('.bi-box-arrow-right');
 function toggledown() {
-  const dropmenu = document.getElementById('drop');
   dropmenu.classList.toggle('show');
+  closeBtn.style.display = 'block';
+  menuIcon.style.display = 'none';
+  fullScreenexit.classList.add('show');
 }
-function toggledown() {
+function closedown() {
+  dropmenu.classList.remove('show');
+  closeBtn.style.display = 'none';
+  menuIcon.style.display = 'block';
+  fullScreenexit.classList.remove('show');
+}
+function closetoggledown() {
   const dropmenu = document.getElementById('drop');
+  const menuIcon = document.querySelector('.bi-menu-button-wide');
+  const closeBtn = document.querySelector('.bi-box-arrow-right');
   if (dropmenu) {
-    dropmenu.classList.toggle('show');
+    dropmenu.classList.remove('show');
+    closeBtn.style.display = 'none';
+    menuIcon.style.display = 'block';
   }
-  const mainImage = document.getElementById('main-image');
-if (dropmenu.classList.contains('show')) {
-  mainImage.src = "IMG_6817.png"; 
-  mainImage.style.display='block';
 }
-else{
-  mainImage.src="IMG_5524.png";
-  mainImage.style.display='block';
+
+
+function updateClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
-}
-  function updateClock() {
-    const now = new Date();
-          const hours = String(now.getHours()).padStart(2, '0');
-          const minutes = String(now.getMinutes()).padStart(2, '0');
-          const seconds = String(now.getSeconds()).padStart(2, '0');
-          document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
-        }
-    setInterval(updateClock, 1000);
-        updateClock(); 
+setInterval(updateClock, 1000);
+updateClock();
 const counter = document.getElementById('counter');
 const targetNumber = 3000;
 const duration = 100000;
 const increment = targetNumber / (duration / 16);
 let currentCount = 0;
+
+
 function updateCounter() {
   currentCount += increment;
-
   if (currentCount >= targetNumber) {
     currentCount = targetNumber;
-    counter.textContent = Math.floor(currentCount); 
+    counter.textContent = Math.floor(currentCount);
     return;
   }
   counter.textContent = Math.floor(currentCount);
@@ -93,6 +110,7 @@ const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 let currentIndex = 0;
 
+
 prevButton.addEventListener('click', () => {
   if (currentIndex > 0) {
     currentIndex--;
@@ -101,7 +119,6 @@ prevButton.addEventListener('click', () => {
   }
   updateCarousel();
 });
-
 nextButton.addEventListener('click', () => {
   if (currentIndex < carousel.children.length - 1) {
     currentIndex++;
